b4d82e142c54ee626ac03e01454ad0a0a3267bfb
Yvan Sanson <32483478+ysanson@users.noreply.github.com>
2023-01-27 09:39:52 +0100

    Version 0.0.1 (#4)
    * Added first component and architecture
    
    * Added eslint
    
    * Add components (#2)
    
    * Create library (#3)
    
    * Added rollup
    
    Signed-off-by: Yvan Sanson <sansonyvan@gmail.com>
    
    * Added rollup config and readme
    
    Signed-off-by: Yvan Sanson <sansonyvan@gmail.com>
    
    * Fixed markdown link
    
    Signed-off-by: Yvan Sanson <sansonyvan@gmail.com>
    
    * Improved components and readme
    
    Signed-off-by: Yvan Sanson <sansonyvan@gmail.com>
    
    * Updated dependencies, rollup config, readme
    
    Signed-off-by: Yvan Sanson <sansonyvan@gmail.com>
    
    Signed-off-by: Yvan Sanson <sansonyvan@gmail.com>
    
    Signed-off-by: Yvan Sanson <sansonyvan@gmail.com>

---
+++ README.md
@@ -0,0 +1,93 @@
+# Simple-form-builder
+
+## Description
+
+This component library aims to help simplify the creation of dynamic forms.
+By using a form definition describing inputs, users can create, store and generate forms to be used in an application.
+
+## Dependencies
+
+This library is based on the fantastic [react-hook-form](https://react-hook-form.com) library for validations and input management.
+
+For the input list, we use the [react-sortablejs](https://github.com/SortableJS/react-sortablejs) library.
+
+The UI is based on Bootstrap, and the [react-bootstrap](https://react-bootstrap.github.io) library. This library required Bootstrap to be loaded in the project.
+
+## Usage
+
+### Installation
+
+Download and install it with `npm i @ysanson/simple-form-builder`. 
+
+### Using the components
+
+#### FormBuilder
+
+In a Function Component, import the FormBuilder component and provide it a form definition to work with:
+```JS
+const FormCreationPage = () => {
+    const [formDefinition, setLocalFormDefinition] = useState<FormInput[]>([]);
+    
+    return <>
+      <FormBuilder formDefinition={formDefinition} updateFormDefinition={(data) => setLocalFormDefinition(data)} />
+    </>
+  }
+```
+
+You may run into problems by using Redux as the state manager, so I recommend using a local state to handle the form definition.
+
+#### FormPreview
+
+The form preview component is there to display the form as it would be in use. It's there so that the necessary props of the FormGenerator component aren't needed.
+
+```JS
+const FormPreviewPage = () => {
+    const [formDefinition, setLocalFormDefinition] = useState<FormInput[]>([]);
+    
+    return <>
+      <FormPreview formDefinition={formDefinition} /> 
+    </>
+  }
+```
+
+#### FormGenerator
+
+The form generator component supports both in-component and props-based submit events, meaning you can either control it from the parent component or leave a Submit button. In all cases, the onSubmitSuccess prop is required. The following is an example of basic use-case where the submit is internal:
+
+```JS
+const FormUserPage = () => {
+    const [formDefinition, setLocalFormDefinition] = useState<FormInput[]>([]);
+    
+    return <>
+      <FormGenerator formDefinition={formDefinition} onSubmitSuccess={(responses: FieldValues) => console.log(responses)} /> 
+    </>
+  }
+
+```
+
+If you want to handle the submit externally, simply fill the hideSubmit and triggerSubmit props:
+
+```JS
+const FormUserPage = () => {
+    const [formDefinition, setLocalFormDefinition] = useState<FormInput[]>([]);
+    const [submitNow, setSubmitNow] = useState(false);
+    
+    return <>
+      <FormGenerator 
+        formDefinition={formDefinition}
+        hideSubmit
+        triggerSubmit={submitNow}
+        onSubmitSuccess={(responses: FieldValues) => console.log(responses)}
+      /> 
+    </>
+  }
+```
+
+## Roamap
+
+- Add tests
+- Add more documentation
+- Improve the creation of Select component
+- Better locale handling
+- Improve performance
+- Add more validation options
---
+++ src/components/SortableContainer/SortableContainer.component.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { FormInput } from "../../types/FormInput"; 
+import { ReactSortable } from "react-sortablejs";
+import ListGroup from "react-bootstrap/ListGroup";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
+
+const CustomLG = React.forwardRef<HTMLDivElement, any>((props, ref) => {
+    return <ListGroup ref={ref}>{props.children}</ListGroup>;
+});
+CustomLG.displayName = "CustomListGroup";
+
+interface SortableContainerProps {
+    updateFormDefinition: (data: FormInput[]) => void;
+    formDefinition: FormInput[];
+    editId: string;
+    setEditId: (payload: string) => void;
+    setEditFormData: (payload: FormInput | undefined) => void;
+    reset: () => void;
+}
+
+const SortableContainer: React.FC<SortableContainerProps> = ({ updateFormDefinition, formDefinition, editId, setEditId, setEditFormData, reset }) => {
+    return (
+        <ReactSortable
+            tag={CustomLG}
+            list={formDefinition}
+            setList={(items) => {
+                updateFormDefinition(items);
+            }}
+        >
+            {formDefinition.map((field) => {
+                return (
+                    <ListGroup.Item key={field.id} className="d-flex justify-content-between align-items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                          <path d="M96 32H32C14.33 32 0 46.33 0 64v64c0 17.67 14.33 32 32 32h64c17.67 0 32-14.33 32-32V64c0-17.67-14.33-32-32-32zm0 160H32c-17.67 0-32 14.33-32 32v64c0 17.67 14.33 32 32 32h64c17.67 0 32-14.33 32-32v-64c0-17.67-14.33-32-32-32zm0 160H32c-17.67 0-32 14.33-32 32v64c0 17.67 14.33 32 32 32h64c17.67 0 32-14.33 32-32v-64c0-17.67-14.33-32-32-32zM288 32h-64c-17.67 0-32 14.33-32 32v64c0 17.67 14.33 32 32 32h64c17.67 0 32-14.33 32-32V64c0-17.67-14.33-32-32-32zm0 160h-64c-17.67 0-32 14.33-32 32v64c0 17.67 14.33 32 32 32h64c17.67 0 32-14.33 32-32v-64c0-17.67-14.33-32-32-32zm0 160h-64c-17.67 0-32 14.33-32 32v64c0 17.67 14.33 32 32 32h64c17.67 0 32-14.33 32-32v-64c0-17.67-14.33-32-32-32z"/>
+                        </svg>
+                        <div className="ms-2 me-auto">
+                            <div className="fw-bold">{field.name}</div>
+                            {field.type}
+                        </div>
+
+                        <div className="edit-panel">
+                            <ButtonGroup size="sm">
+                                <Button
+                                    active={editId === field.id}
+                                    variant="outline-primary"
+                                    onClick={() => {
+                                        if (editId === field.id) {
+                                            setEditId("");
+                                            setEditFormData(undefined);
+                                            reset();
+                                        } else {
+                                            const index = formDefinition.findIndex((data) => field.name === data.name);
+                                            setEditFormData(formDefinition[index]);
+                                            setEditId(field.id);
+                                        }
+                                    }}
+                                >
+                                    {editId === field.id ? "Annuler" : "Modifier"}
+                                </Button>
+                                <Button
+                                    variant="outline-danger"
+                                    onClick={() => {
+                                        if (window.confirm("Voulez-vous vraiment supprimer ce champ ?")) {
+                                            const index = formDefinition.findIndex((data) => field.name === data.name);
+                                            if (editId === field.id) {
+                                                setEditFormData(undefined);
+                                            }
+
+                                            if (index >= 0) {
+                                                updateFormDefinition([...formDefinition.slice(0, index), ...formDefinition.slice(index + 1)]);
+                                                setEditId("");
+                                            }
+                                        }
+                                    }}
+                                >
+                                    Supprimer
+                                </Button>
+                            </ButtonGroup>
+                        </div>
+                    </ListGroup.Item>
+                );
+            })}
+        </ReactSortable>
+    );
+};
+
+export default SortableContainer;
+
