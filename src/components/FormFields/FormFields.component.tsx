14e2bf2d5c91b458acdaf3e9faf713d7a61540fa
Yvan Sanson <32483478+ysanson@users.noreply.github.com>
2023-01-27 14:01:54 +0100

    Improve doc and linter (#7)
    * Corrected eslint config, ran eslint format, updated readme typo
    
    Signed-off-by: Yvan Sanson <sansonyvan@gmail.com>
    
    * Added install command description
    
    Signed-off-by: Yvan Sanson <sansonyvan@gmail.com>
    
    Signed-off-by: Yvan Sanson <sansonyvan@gmail.com>

--- src/components/FormFields/FormFields.component.tsx
+++ src/components/FormFields/FormFields.component.tsx
@@ -13,153 +13,153 @@ interface FormFieldsProps {
 }
 
 const FormFields: React.FC<FormFieldsProps> = ({ fields, errors, register }) => {
-    const getSelectOptions = (input: FormInput): SelectOption[] => {
-        if (input.options) {
-            return input.options.map((opt) => ({ label: opt, value: opt }));
+  const getSelectOptions = (input: FormInput): SelectOption[] => {
+    if (input.options) {
+      return input.options.map((opt) => ({ label: opt, value: opt }));
+    } else {
+      return [];
+    }
+  };
+
+  const getLabel = (input: FormInput): string => {
+    if (input.label && input.label !== "") {
+      return input.label;
+    } else {
+      return input.name;
+    }
+  };
+
+  const getDescription = (description: string | undefined) => {
+    if (description && description !== "") {
+      return <Form.Text>{description}</Form.Text>;
+    } else {
+      return <></>;
+    }
+  };
+
+  const createElement = useCallback(
+    (input: FormInput) => {
+      switch (input.type) {
+      case "header":
+        return <h3 className="text-center"> {getLabel(input)} </h3>;
+      case "subheader":
+        return (
+          <Row key={input.id} className="mb-3">
+            <h5 className="text-center">{getLabel(input)}</h5>
+          </Row>
+        );
+      case "boolean":
+        return (
+          <Row key={input.id} className="mb-3">
+            <Form.Group>
+              <Form.Check
+                label={getLabel(input)}
+                isInvalid={!!errors[input.name]}
+                defaultChecked={input.defaultValue?.toLowerCase() === "true"}
+                {...register(input.name, { required: input.required })}
+              />
+              {getDescription(input.description)}
+              <Form.Control.Feedback type="invalid">Ce champ est requis.</Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+        );
+      case "select":
+        return (
+          <Row key={input.id} className="mb-3">
+            <Form.Group>
+              <Select
+                id={`select-${input.id}`}
+                options={getSelectOptions(input)}
+                label={getLabel(input)}
+                isInvalid={!!errors[input.name]}
+                {...register(input.name, { required: input.required })}
+              />
+              {getDescription(input.description)}
+              <Form.Control.Feedback type="invalid">Ce champ est requis.</Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+        );
+      case "number":
+        return (
+          <Row key={input.id} className="mb-3">
+            <Form.Group controlId={`input-${input.id}`}>
+              <Form.Label>{getLabel(input)}</Form.Label>
+              <Form.Control
+                type="number"
+                isInvalid={!!errors[input.name]}
+                defaultValue={input.defaultValue}
+                {...register(input.name, { required: input.required, valueAsNumber: true })}
+              />
+              {getDescription(input.description)}
+              <Form.Control.Feedback type="invalid">Ce champ est requis.</Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+        );
+      case "text":
+        return (
+          <Row key={input.id} className="mb-3">
+            <Form.Group controlId={`input-${input.id}`}>
+              <Form.Label>{getLabel(input)}</Form.Label>
+              <Form.Control
+                isInvalid={!!errors[input.name]}
+                defaultValue={input.defaultValue}
+                {...register(input.name, { required: input.required })}
+              />
+              {getDescription(input.description)}
+              <Form.Control.Feedback type="invalid">Ce champ est requis.</Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+        );
+      case "textarea":
+        return (
+          <Row key={input.id} className="mb-3">
+            <Form.Group controlId={`input-${input.id}`}>
+              <Form.Label>{getLabel(input)}</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                isInvalid={!!errors[input.name]}
+                defaultValue={input.defaultValue}
+                {...register(input.name, { required: input.required })}
+              />
+              {getDescription(input.description)}
+              <Form.Control.Feedback type="invalid">Ce champ est requis.</Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+        );
+      default:
+        return <></>;
+      }
+    },
+    [errors, register]
+  );
+
+  const createFields = () => {
+    const setId = new Set<string>();
+
+    return fields.map((input, index) => {
+      if (!setId.has(input.id)) {
+        setId.add(input.id);
+        const el = createElement(input);
+        if (input.type.includes("header") || !fields[index + 1]) {
+          return <Row key={input.id}>{el}</Row>;
         } else {
-            return [];
+          const secondEl = createElement(fields[index + 1]);
+          setId.add(fields[index + 1].id);
+          return (
+            <Row key={input.id}>
+              <Col>{el}</Col>
+              <Col>{secondEl}</Col>
+            </Row>
+          );
         }
-    };
+      } else {
+        return <></>;
+      }
+    });
+  };
 
-    const getLabel = (input: FormInput): string => {
-        if (input.label && input.label !== "") {
-            return input.label;
-        } else {
-            return input.name;
-        }
-    };
-
-    const getDescription = (description: string | undefined) => {
-        if (description && description !== "") {
-            return <Form.Text>{description}</Form.Text>;
-        } else {
-            return <></>;
-        }
-    };
-
-    const createElement = useCallback(
-        (input: FormInput) => {
-            switch (input.type) {
-                case "header":
-                    return <h3 className="text-center"> {getLabel(input)} </h3>;
-                case "subheader":
-                    return (
-                        <Row key={input.id} className="mb-3">
-                            <h5 className="text-center">{getLabel(input)}</h5>
-                        </Row>
-                    );
-                case "boolean":
-                    return (
-                        <Row key={input.id} className="mb-3">
-                            <Form.Group>
-                                <Form.Check
-                                    label={getLabel(input)}
-                                    isInvalid={!!errors[input.name]}
-                                    defaultChecked={input.defaultValue?.toLowerCase() === "true"}
-                                    {...register(input.name, { required: input.required })}
-                                />
-                                {getDescription(input.description)}
-                                <Form.Control.Feedback type="invalid">Ce champ est requis.</Form.Control.Feedback>
-                            </Form.Group>
-                        </Row>
-                    );
-                case "select":
-                    return (
-                        <Row key={input.id} className="mb-3">
-                            <Form.Group>
-                                <Select
-                                    id={`select-${input.id}`}
-                                    options={getSelectOptions(input)}
-                                    label={getLabel(input)}
-                                    isInvalid={!!errors[input.name]}
-                                    {...register(input.name, { required: input.required })}
-                                />
-                                {getDescription(input.description)}
-                                <Form.Control.Feedback type="invalid">Ce champ est requis.</Form.Control.Feedback>
-                            </Form.Group>
-                        </Row>
-                    );
-                case "number":
-                    return (
-                        <Row key={input.id} className="mb-3">
-                            <Form.Group controlId={`input-${input.id}`}>
-                                <Form.Label>{getLabel(input)}</Form.Label>
-                                <Form.Control
-                                    type="number"
-                                    isInvalid={!!errors[input.name]}
-                                    defaultValue={input.defaultValue}
-                                    {...register(input.name, { required: input.required, valueAsNumber: true })}
-                                />
-                                {getDescription(input.description)}
-                                <Form.Control.Feedback type="invalid">Ce champ est requis.</Form.Control.Feedback>
-                            </Form.Group>
-                        </Row>
-                    );
-                case "text":
-                    return (
-                        <Row key={input.id} className="mb-3">
-                            <Form.Group controlId={`input-${input.id}`}>
-                                <Form.Label>{getLabel(input)}</Form.Label>
-                                <Form.Control
-                                    isInvalid={!!errors[input.name]}
-                                    defaultValue={input.defaultValue}
-                                    {...register(input.name, { required: input.required })}
-                                />
-                                {getDescription(input.description)}
-                                <Form.Control.Feedback type="invalid">Ce champ est requis.</Form.Control.Feedback>
-                            </Form.Group>
-                        </Row>
-                    );
-                case "textarea":
-                    return (
-                        <Row key={input.id} className="mb-3">
-                            <Form.Group controlId={`input-${input.id}`}>
-                                <Form.Label>{getLabel(input)}</Form.Label>
-                                <Form.Control
-                                    as="textarea"
-                                    rows={3}
-                                    isInvalid={!!errors[input.name]}
-                                    defaultValue={input.defaultValue}
-                                    {...register(input.name, { required: input.required })}
-                                />
-                                {getDescription(input.description)}
-                                <Form.Control.Feedback type="invalid">Ce champ est requis.</Form.Control.Feedback>
-                            </Form.Group>
-                        </Row>
-                    );
-                default:
-                    return <></>;
-            }
-        },
-        [errors, register]
-    );
-
-    const createFields = () => {
-        const setId = new Set<string>();
-
-        return fields.map((input, index) => {
-            if (!setId.has(input.id)) {
-                setId.add(input.id);
-                const el = createElement(input);
-                if (input.type.includes("header") || !fields[index + 1]) {
-                    return <Row key={input.id}>{el}</Row>;
-                } else {
-                    const secondEl = createElement(fields[index + 1]);
-                    setId.add(fields[index + 1].id);
-                    return (
-                        <Row key={input.id}>
-                            <Col>{el}</Col>
-                            <Col>{secondEl}</Col>
-                        </Row>
-                    );
-                }
-            } else {
-                return <></>;
-            }
-        });
-    };
-
-    return <> {createFields()} </>;
+  return <> {createFields()} </>;
 };
 
 export default FormFields;
